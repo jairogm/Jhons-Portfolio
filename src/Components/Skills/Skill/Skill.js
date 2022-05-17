169d0af50bd1a0b6e7a082022c85aecb28b03afb
Jhon Jairo Garces Montes <jairogarcesmontes@MacBook-Pro-de-Jhon.local>
2022-05-16 20:40:10 -0500

    Finish up about page

--- src/Components/Skills/Skill/Skill.js
+++ src/Components/Skills/Skill/Skill.js
@@ -2,6 +2,10 @@ import { StyledSkill } from "./Skill.styled"
 export default function Skill({ skill, borderColor, logo }) {
 
     return (
-        <StyledSkill borderColor={borderColor}><img src={logo} alt={skill + " logo"} /><span>{skill}</span></StyledSkill>
+        <StyledSkill borderColor={borderColor}>
+            {/* <img src={logo} alt={skill + " logo"} /> */}
+        
+        <span>{logo}</span>
+        <span>{skill}</span></StyledSkill>
     )
 }
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,9 +4,9 @@ import { Container, Section } from "../Components"
 import GlobalStyles from "../GlobalStyles"
 import styled from "styled-components"
 import SocialIcons from "../Components/SocialIcons/SocialIcons"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import getRandomItem from "../Services/getRandomItem"
-
+import { FiMaximize, FiMinimize } from "react-icons/fi"
 const Image = styled.div` 
   width: 100%;
   height: 300px;
@@ -29,25 +29,83 @@ const Image = styled.div`
     object-fit: cover;
   }
 
+  & > button {
+    width: 30px;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: #c4c4c4;
+    color: #fff;
+    border-radius:5px;
+    padding: 5px;
+    cursor: pointer;
+    border: none;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    overflow: hidden;
+    transition: width 1s;
+    &:hover {
+      width:80px;
+      
+      
+    }
+    
+    &:hover > span {
+      
+      display: block;
+      transition:  0.5s ease-in-out;
+    }
+    
+    & > span {
+      display: none;
+      transition: all 0.5s ease-in-out;
+    }
+  }
+
+
+
+  transition: all 0.5s ease;
+  
+ ${({ active }) => active && `
+    transition: all 0.5s ease;
+    height: 500px;
+  `}
+
 `
 
 const ImageList = () => [
   {
     key: "0",
-    alt: "Me and some friends haging up at Manhattan - New York",
+    alt: "Me and some friends hanging out at Central Park - New York City, New York - 2021",
   },
   {
     key: "1",
-    alt: "Visiting the Capitol - Washington, DC",
+    alt: "Visiting the Capitol - Washington, DC - 2021",
   },
   {
     key: "2",
-    alt: "Community College Initiative Colombian Group - Bogota, Colombia",
+    alt: "Community College Initiative Program Colombian Group - Bogota, Colombia - 2021",
+  },
+  {
+    key: "3",
+    alt: "Learning how to paint with friends - Alexandria, Virginia - 2021",
+  },
+  {
+    key: "4",
+    alt: "Sharing a Coffee with Friends at Hard Rock Cafe - San Francisco, California - 2018",
+  },
+  {
+    key: "5",
+    alt: "Playing with snow for the first time in life - Alexandria, Virginia - 2021",
   },
 ]
 
 
 export function About() {
+
+  const [OpenImg, setOpenImg] = useState(false)
   const getImage = useMemo(() => {
 
     return getRandomItem(ImageList())
@@ -63,10 +121,13 @@ export function About() {
 
       <Section>
         <h3>About Me</h3>
-        <Image>
-          <img  src={require(`../Assets/about/ab-${getImage.key}.jpg`)} alt={getImage.alt} />
+        <Image active={OpenImg}>
+          <img src={require(`../Assets/about/ab-${getImage.key}.jpg`)} alt={getImage.alt} />
           {/* Caption */}
           <small>{getImage.alt}</small>
+          {!OpenImg ? <button onClick={() => setOpenImg(!OpenImg)}> <FiMaximize size={20} /><span>Resize</span></button>
+            : <button onClick={() => setOpenImg(!OpenImg)}> <FiMinimize size={20} /><span>Close</span></button>
+          }
         </Image>
       </Section>
       <Section>
