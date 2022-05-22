5656089348336931763b3b58c154684960e1e2f7
Jhon Garces Montes <jairogarces1@gmail.com>
2022-05-22 15:18:39 -0500

    Add Now Playing spotify on Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,18 +1,24 @@
 import { Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Flex } from '../layouts/Flex'
 import SocialIcons from '../SocialIcons/SocialIcons'
 import { StyledFooter } from './Footer.styled'
 import { FaSpotify } from "react-icons/fa"
-
+import { getNowPlaying } from '../../lib/spotify'
 export default function Footer() {
+
+    const [data, setData] = useState(null)
+    useEffect(() => {
+        getNowPlaying().then(res => res.json()).then(data => setData(data))
+    }, [])
+
     return (
         <StyledFooter>
             <Flex mobileDirection="column" align="center" justify="space-between" gap="10px" margin="0 0 40px">
                 <Flex align="center" gap="5px">
                     <FaSpotify size={20} />
                     <Text>
-                        <b> Not playing</b>
+                        <b>{data ? <>{data.item.name} - {data.item.artists[0].name}</> : "Not Playing"}</b>
                     </Text>
                     <Text color="#888"> - Spotify</Text>
                 </Flex>
