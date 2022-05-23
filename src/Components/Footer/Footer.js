7ab47dfe867ea088f6df7086b24a1fbeb4ba1643
Jhon Garces Montes <jairogarces1@gmail.com>
2022-05-22 22:40:50 -0500

    fix endpoint

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,19 +8,21 @@ import { getNowPlaying } from '../../lib/spotify'
 export default function Footer() {
 
     const [data, setData] = useState(null)
+
     useEffect(() => {
 
-        getNowPlaying().then(res => res.json()).then(data => setData(data)).catch(err=>console.log(err))
+        getNowPlaying().then(res => res.json())
+        .then(data => setData(data))
+        .catch(err => console.log(err))
 
     }, [])
-console.log(data)
     return (
         <StyledFooter>
             <Flex mobileDirection="column" align="center" justify="space-between" gap="10px" margin="0 0 40px">
                 <Flex align="center" gap="5px">
                     <FaSpotify size={20} />
                     <Text>
-                        <b>{data ? <>{data.item.name} - {data.item.artists[0].name}</> : "Not Playing"}</b>
+                        <b>{data ? <>{data.item.name} - {data.item.artists.map((artist, index)=> artist.name).join(', ')}</> : "Not Playing"}</b>
                     </Text>
                     <Text color="#888"> - Spotify</Text>
                 </Flex>
