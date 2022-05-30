0dfce8fbad99c96960f7ce2497ccf9b40b01e562
Jhon Garces Montes <jairogarces1@gmail.com>
2022-05-29 21:37:08 -0500

    Update Projects constants

--- src/utils/Constants/Projects.constant.js
+++ src/utils/Constants/Projects.constant.js
@@ -4,40 +4,40 @@ export const Projects = [
 
         "title": "Tasky",
         "description": "Taks app on the Ethereum blockchain",
-        "demoLink": "https://www.example.com",
-        "sourceCode": "https://www.github.com",
+        "demoLink": "https://tasky.jhongarces.dev",
+        "sourceCode": "https://github.com/jairogm/tasky-app",
         "borderColor": "linear-gradient(90deg, rgba(216,180,254,1) 0%, rgba(129,140,248,1) 100%)"
 
     },
 
-    {
-        "title": "Cuacane",
-        "description": "Forecast app dashboard",
-        "demoLink": "https://www.example.com",
-        "sourceCode": "https://www.github.com",
-        "borderColor": "linear-gradient(90deg, rgba(110,231,183,1) 0%, rgba(59,130,246,1) 50%, rgba(147,51,234,1) 100%)"
-    },
-    {
-        "title": "Simple CRUD",
-        "description": "brief description of the project",
-        "demoLink": "https://www.example.com",
-        "sourceCode": "https://www.github.com",
-        "borderColor": " linear-gradient(90deg, rgba(253,230,138,1) 0%, rgba(252,165,165,1) 50%, rgba(254,202,202,1) 100%)"
+    // {
+    //     "title": "Cuacane",
+    //     "description": "Forecast app dashboard",
+    //     "demoLink": "https://www.example.com",
+    //     "sourceCode": "https://www.github.com",
+    //     "borderColor": "linear-gradient(90deg, rgba(110,231,183,1) 0%, rgba(59,130,246,1) 50%, rgba(147,51,234,1) 100%)"
+    // },
+    // {
+    //     "title": "Simple CRUD",
+    //     "description": "brief description of the project",
+    //     "demoLink": "https://www.example.com",
+    //     "sourceCode": "https://www.github.com",
+    //     "borderColor": " linear-gradient(90deg, rgba(253,230,138,1) 0%, rgba(252,165,165,1) 50%, rgba(254,202,202,1) 100%)"
 
-    },
-    {
-        "title": "Pinterest Clone",
-        "description": "brief description of the project",
-        "demoLink": "https://www.example.com",
-        "sourceCode": "https://www.github.com",
-        "borderColor": "linear-gradient(90deg, rgba(216,180,254,1) 0%, rgba(129,140,248,1) 100%)"
-    },
-    {
-        "title": "Radio App",
-        "description": "brief description of the project",
-        "demoLink": "https://www.example.com",
-        "sourceCode": "https://www.github.com",
-        "borderColor": "linear-gradient(90deg, rgba(110,231,183,1) 0%, rgba(59,130,246,1) 50%, rgba(147,51,234,1) 100%)"
-    },
+    // },
+    // {
+    //     "title": "Pinterest Clone",
+    //     "description": "brief description of the project",
+    //     "demoLink": "https://www.example.com",
+    //     "sourceCode": "https://www.github.com",
+    //     "borderColor": "linear-gradient(90deg, rgba(216,180,254,1) 0%, rgba(129,140,248,1) 100%)"
+    // },
+    // {
+    //     "title": "Radio App",
+    //     "description": "brief description of the project",
+    //     "demoLink": "https://www.example.com",
+    //     "sourceCode": "https://www.github.com",
+    //     "borderColor": "linear-gradient(90deg, rgba(110,231,183,1) 0%, rgba(59,130,246,1) 50%, rgba(147,51,234,1) 100%)"
+    // },
 
 ]
