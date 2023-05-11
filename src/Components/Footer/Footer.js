6655b1524ddbcc592016a8ba5d5b5128469ab8f6
Jhon Jairo Garces Montes <68207789+jairogm@users.noreply.github.com>
2023-05-11 18:44:22 -0500

    latest

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,11 @@
 import { Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Flex } from '../layouts/Flex'
-import SocialIcons from '../SocialIcons/SocialIcons'
-import { Anchor, StyledFooter } from './Footer.styled'
-import { FaSpotify } from "react-icons/fa"
-import { getNowPlaying } from '../../lib/spotify'
+//import SocialIcons from '../SocialIcons/SocialIcons'
+import { StyledFooter } from './Footer.styled'
+//import { FaSpotify } from "react-icons/fa"
+//import { getNowPlaying } from '../../lib/spotify'
 export default function Footer() {
-
-    const [data, setData] = useState(null)
 
     // useEffect(() => {
 
